b1b219f770bba5bf20ba01c55ed8aff3d40b1694
deepak kumar <deepak7292832956@gmail.com>
2026-02-04 20:33:15 +0530

    added assets

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,66 @@
+import project1_img from '../assets/project_1.png'
+import project2_img from '../assets/project_2.png'
+import project3_img from '../assets/project_3.png'
+import project4_img from '../assets/project_4.png'
+import project5_img from '../assets/project_5.svg'
+import project6_img from '../assets/project_6.svg'
+import project7_img from '../assets/project_4.svg'
+import project8_img from '../assets/project_3.svg'
+import project9_img from '../assets/project_2.svg'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_img: project1_img,
+        link: "https://tomato-food-app-frontend-l2ni.onrender.com",
+        title: "Tomato Food App"
+       
+    },
+    {
+        w_no:2,
+        w_img: project2_img,
+        link: "https://quickstay-hotels-app.vercel.app/",
+        title: "QuickStay Hotel App"
+    },
+    {
+        w_no:3,
+        w_img:project3_img,
+        link: "https://just-chat-cyan.vercel.app/",
+        title: "Just Chat App"
+    },
+    {
+        w_no:4,
+        w_img:project4_img,
+        link: "https://shoping-app-ashen.vercel.app/",
+        title: "Shopping App"
+    },
+    {
+        w_no:5,
+        w_name:"Web design",
+        w_img:project5_img
+    },
+    {
+        w_no:6,
+        w_name:"Web design",
+        w_img:project6_img
+    },
+    {
+        w_no:7,
+        w_name:"Web design",
+        w_img:project7_img
+    },
+    {
+        w_no:8,
+        w_name:"Web design",
+        w_img:project8_img
+    },
+    {
+        w_no:9,
+        w_name:"Web design",
+        w_img:project9_img
+    },
+    
+]
+ 
+export default mywork_data;
+
---
+++ src/assets/services_data.js
@@ -0,0 +1,35 @@
+const Services_Data = [
+  {
+    s_no: "01",
+    s_name: "Web design",
+    s_desc: "creating visually appealing, user-friendly, and responsive websites for a seamless user experience.",
+  },
+  {
+    s_no: "02",
+    s_name: "Educator",
+    s_desc: "As an educator, I offer courses and guidance on web design, content writing, and social media.",
+  },
+  {
+    s_no: "03",
+    s_name: "Social media",
+    s_desc: "I create engaging social media content, manage posts, and strategies to boost audience engagement.",
+  },
+  {
+    s_no: "04",
+    s_name: "Backend Development",
+    s_desc: "Managing databases, authentication, and server-side logic using Node.js .",
+  },
+  {
+    s_no: "05",
+    s_name: "UI/UX Design",
+    s_desc: "Designing wireframes and prototypes for seamless user experiences.",
+  },
+  {
+    s_no: "06",
+    s_name: "Content writing",
+    s_desc:
+      "I am also a content writer who crafts clear, engaging, and impactful content. ",
+  },
+];
+
+export default Services_Data;
